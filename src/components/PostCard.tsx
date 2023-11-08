9f4f23afd89bf2e0c60155ad1455e8f0f09e74fa
spoonschad <chapo@membot.tools>
2023-11-08 18:15:26 -0500

    mmmd

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -58,7 +58,7 @@ export const PostCard: React.FC<{ commentCount?: number, hideComments: boolean, 
                 </IonButton>
 
                 <IonLabel style={{ position: 'absolute', marginTop: 1, paddingLeft: 2, right: 40, paddingRight: 2 }} >
-                    <IonText className='heavy'>{score} </IonText>
+                    <IonText className='bold'>{score} </IonText>
                 </IonLabel>
 
                 <IonButton style={{ position: 'absolute', right: 0 }} fill='clear' onPointerDown={() => handleVote(id, uid, true)} color={typeof voted !== 'undefined' && voted !== null && voted === 1 ? 'success' : 'medium'}>
