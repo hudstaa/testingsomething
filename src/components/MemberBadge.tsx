a91f56cb3885cf77627edb80c4deafa91a324d30
spoonschad <chapo@membot.tools>
2023-11-08 15:35:28 -0500

    x

--- src/components/MemberBadge.tsx
+++ src/components/MemberBadge.tsx
@@ -79,7 +79,7 @@ export const MemberCardHeader: React.FC<{ address: string, color?: string, conte
             <img style={{ width: 35, height: 35, borderRadius: '10px', marginLeft: 0 }} src={member?.twitterPfp || personOutline} />
         </IonRouterLink>        
         <IonGrid fixed style={{ paddingLeft: 7 }}>
-            <IonRow style={{ marginTop: -2, marginBottom: '2px' }}> {/* Reduced bottom margin */}
+            <IonRow style={{ marginTop: -3, marginBottom: '2px' }}> {/* Reduced bottom margin */}
                 <IonRouterLink routerLink={'/member/' + address}>
                     <IonText color='dark' className='bold' style={{ fontSize: '14px' , letterSpacing: '-0.035em' }}>
                         {member?.twitterName}
