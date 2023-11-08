d8e0aa0333e5feb68fa668fab3387daebf84e884
spoonschad <chapo@membot.tools>
2023-11-08 16:23:23 -0500

    messaging edits

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -7,41 +7,60 @@ import { timeAgo } from "./TradeItem";
 
 export const NewChatBubble: React.FC<{ message: Message, me: string, channel: string, reply: (messageId: string) => void }> = ({ message, me, channel, reply }) => {
     const isMe = me === message.author;
-    const contentBubble = !message.media ? <div style={{ margin: 0 }} className={(isMe ? "send" : "recieve") + ' msg regular'}>
-        {message.content}
-    </div> : <div style={{ margin: 0, padding: 0 }}>
-        <img className={(isMe ? "send" : "recieve") + ' msg image-msg'} height={'200px'} src={message.media.src} />
-        {message.content && <div style={{ margin: 0 }} className={(isMe ? "send" : "recieve") + ' msg regular'}>
-            {message.content}
-        </div>}
-    </div>
 
-    const items = [
-        !isMe ? <button style={{ margin: '0px!important', padding: 0, paddingRight: 5, paddingLeft: 5, background: 'rgba(0,0,0,0)' }} onClick={() => {
-            reply(message.id);
-        }} color='primary' >
-            <IonIcon style={{ margin: 0 }} color='tertiary' icon={returnDownBack} />
-        </button> : undefined
-        , message.reply ? <RenderReply messageId={message.reply} channel={channel} me={me} isReplyToMe={isMe} />
-            : undefined,
-        <div style={{ margin: '0px!important' }}>
-            {contentBubble}
+    const bubbleAndButtonContainerStyle = {
+        display: 'flex',
+        alignItems: 'center', // Vertically align the content bubble and reply button
+        // Adjust the width to account for the profile picture
+    };
+
+    const contentBubble = (
+        <div style={{ maxWidth: '70%', alignSelf: isMe ? 'flex-end' : 'flex-start' }}>
+            {message.media && (
+                <img className={(isMe ? "send" : "recieve") + ' msg image-msg'} style={{ maxWidth: '100%', height: 'auto' }} src={message.media.src} />
+            )}
+            <div className={(isMe ? "send" : "recieve") + ' msg regular'} style={{ margin: 0 }}>
+                {message.content}
+            </div>
         </div>
-        , isMe ? <></> :
-            <ChatMemberPfp size="smol" address={message.author} style={{ margin: '0px!important', width: '50px!important', height: '50px!important' }} />
-        ,
+    );
 
+    const replyButton = !isMe && (
+        <button
+            style={{ margin: '0px!important', padding: '0px!important', background: 'rgba(0,0,0,0)' }}
+            onClick={() => reply(message.id)}
+            color='primary'
+        >
+            <IonIcon color='tertiary' icon={returnDownBack} />
+        </button>
+    );
 
-
-    ]
-
-    if (!isMe) {
-        items.reverse();
-    }
-    return <div className="message-container" key={message.id} style={{ margin: '0px!important', alignItems: isMe ? 'right' : 'left', display: 'flex', justifyContent: isMe ? 'end' : 'start' }} >
-        {items}
-    </div >
+    const profilePic = isMe ? <></> : (
+        <ChatMemberPfp
+            size="smol"
+            address={message.author}
+            style={{ margin: '0px!important', width: '50px!important', height: '50px!important' }}
+        />
+    );
+    return (
+        <div className="message-container" key={message.id} style={{
+            margin: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: isMe ? 'flex-end' : 'flex-start',
+        }} >
+            {/* Render the reply if present */}
+            {message.reply && <RenderReply messageId={message.reply} channel={channel} me={me} isReplyToMe={isMe} />}
+            {/* Render the message content with the profile picture */}
+            <div style={{ display: 'flex', flexDirection: isMe ? 'row-reverse' : 'row' }}>
+                {profilePic}
+                {contentBubble}
+            </div>
+            {replyButton}
+        </div>
+    );
 }
+
 
 export const RenderReply: React.FC<{ messageId: string, channel: string, isReplyToMe: boolean, me: string }> = ({ messageId, channel, me, isReplyToMe }) => {
     const message = useGroupMessages(x => x.replyMessages[channel].find(x => x.id === messageId) || x.groupMessages[channel].find(x => x.id === messageId))
