7243b78757141d0c55d81f752f5084fd55c544f3
~ . ~ <81006672+lilesper@users.noreply.github.com>
2023-12-12 19:30:18 -0500

    wow

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -53,7 +53,7 @@ export const PostCard: React.FC<{ commentCount?: number, hideComments: boolean, 
             {media && (
                 <div style={{ marginTop: 10, marginBottom: -10, marginRight: 0, overflow: 'hidden', borderRadius: '10px' }}>
                     {media.type.includes("image") ?
-                        <img style={{ minWidth: '100%', maxHeight: 1000, borderRadius: 10 }} src={media.src} /> : <video preload="metadata" autoPlay={showComments} style={{ minHeight: '100%', width: '100%', borderRadius: 10 }} controls={showComments} onClick={()=>{
+                        <img style={{ minWidth: '100%', maxHeight: 1000, borderRadius: 10 }} src={media.src} /> : <video  webkit-playsinline playsInline preload="metadata" autoPlay={showComments} style={{ minHeight: '100%', width: '100%', borderRadius: 10 }} controls={showComments} onClick={()=>{
                             !showComments&&            push('/post/' + id);
 
                         }} src={media.src + '#t=0.6'} onPlay={(e: any) => { e.target.currentTime = 0 }} />}
