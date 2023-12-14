52483e00ba68831065e3659b3e79380084c00b58
spoonschad <chapo@membot.tools>
2023-12-14 17:11:40 -0500

    wd

--- src/components/MemberBadge.tsx
+++ src/components/MemberBadge.tsx
@@ -162,7 +162,7 @@ export const MemberCardHeader: React.FC<{ clickable?: boolean, address: string, 
                         </IonText>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'row', cursor: 'pointer', fontSize: '16px', margin: 0, paddingBottom: 0, letterSpacing: '-0.0235em' }}>
-                    <IonText color='dark' className='regular' style={{ fontSize: 14, paddingLeft: 0, paddingTop: 1, opacity: '75%' }}>
+                    <IonText color='dark' className='regular' style={{ fontSize: 12, paddingLeft: 0, paddingTop: 1, opacity: '75%' }}>
                             @{member?.twitterUsername}
                         </IonText>
                     </div>
