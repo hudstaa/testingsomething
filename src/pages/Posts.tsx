3c5fd7cd10dfd377e9783320a9b4eb84f4906668
spoonschad <chapo@membot.tools>
2023-12-19 15:54:52 -0500

    dw

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -128,7 +128,7 @@ const Posts: React.FC = () => {
                                 <IonText className='medium' style={{ fontSize: '16px', paddingTop: 0, paddingRight: 4 }} color='dark'>
                                     {notifs}
                                 </IonText>
-                                <IonIcon color='dark' size="small" icon={'/icons/noti.svg'} />
+                                <IonIcon icon={'/icons/notialt.svg'}  style={{ height: 24, width: 24 }}/>
                             </IonBadge>
                         </IonButton>
                         <IonButton onMouseDown={() => {
