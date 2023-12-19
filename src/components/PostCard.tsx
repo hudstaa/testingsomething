22db531e144000756265ce9042bac381ef04a4d1
spoonschad <chapo@membot.tools>
2023-12-18 21:07:58 -0500

    dw

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -50,7 +50,7 @@ export const PostCard: React.FC<{ commentCount?: number, hideComments: boolean, 
             </div>
         </IonCardHeader>
         <IonCardContent style={{ paddingLeft: 16, paddingBottom: 1, paddingTop: 0, margin: 0, marginTop: -4 }}  >
-            <IonText color='dark' className='regular' style={{ whiteSpace: 'pre-wrap', fontSize: '1rem', lineHeight: '1', letterSpacing: "-0.0135em" }} onClick={() => {
+            <IonText color='dark' className='regular' style={{ whiteSpace: 'pre-wrap', fontSize: '1.1rem', letterSpacing: "-0.0135em" }} onClick={() => {
             }} >
                 <Linkify options={{
                     render:({attributes,content,eventListeners,tagName})=>{
