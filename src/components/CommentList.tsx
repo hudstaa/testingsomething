a40a8f924ca1097b4e9420dda141a5d27887a7c8
spoonschad <chapo@membot.tools>
2023-12-18 19:14:40 -0500

    d

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -62,7 +62,7 @@ export const CommentList: React.FC<CommentListProps> = ({ postId, amount, uid, o
 
                     <IonItem lines="none" color={'lightt'} style={{ marginTop: 0, marginLeft: -3, marginBottom: 0, paddingTop: 5, paddingBottom: 5 }} >
 
-                        <IonButtons slot='start' style={{ position: 'absolute', paddingLeft: 45, top: 3, fontSize: 14}}>
+                        <IonButtons slot='start' style={{ position: 'absolute', paddingLeft: 48, top: 3, fontSize: 14}}>
                         <MemberAlias color='dark' address={comment.author} />
                         <MemberUsername color='medium' address={comment.author}/>
                         <IonText color={'dark'} className='regular' style={{ paddingBottom: 0, marginTop: '.8px', marginLeft: 3, bottom: 0, fontSize: 10, opacity: '75%' }}>
@@ -72,9 +72,9 @@ export const CommentList: React.FC<CommentListProps> = ({ postId, amount, uid, o
                             }} style={{ margin: 0, padding: 0, paddingLeft: 4, fontSize: 9 }}>Reply</span> */}
                         </IonText>
                         </IonButtons>
-                        <div style={{position: 'absolute', top: 3, borderRadius: 100}}><MemberPfp color='dark' size="smol" style={{ top: 0}}address={comment.author} />
+                        <div style={{position: 'absolute', top: 3, borderRadius: 100}}><MemberPfp color='dark' size="veru-smol" style={{ top: 0}}address={comment.author} />
                         </div>
-                        <IonText className='regular' color='dark' style={{ paddingLeft:45, whitespace: 'pre-wrap', marginTop: 25, marginLeft: 0, marginBottom: 5, fontSize: "1rem", letterSpacing: '-0.0135em'}} >
+                        <IonText className='regular' color='dark' style={{ paddingLeft:48, whitespace: 'pre-wrap', marginTop: 25, marginLeft: 0, marginBottom: 5, fontSize: "1rem", letterSpacing: '-0.0135em'}} >
                             <Linkify>
                                 {comment.content}
                             </Linkify>
