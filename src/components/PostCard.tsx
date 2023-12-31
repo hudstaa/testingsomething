b51da98f1513c23cf02cbffff3bea1f9d303445e
spoonschad <chapo@membot.tools>
2023-12-31 15:38:25 -0500

    dw

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -133,7 +133,7 @@ export const PostCard: React.FC<{ onPostPage?: boolean, commentCount?: number, h
                 
             </div>
         </IonCardHeader>
-        <IonCardContent style={{ paddingLeft: 50, marginLeft: 0, paddingBottom: 1, paddingTop: 0, margin: 0, paddingRight: 16,  marginTop: -12 }}  >
+        <IonCardContent style={{ paddingLeft: 50, marginLeft: 0, paddingBottom: 1, paddingTop: 0, margin: 0, paddingRight: 16,  marginTop: -14 }}  >
             <div>
             <IonText color='dark' className='regular' style={{ whiteSpace: 'pre-wrap', fontSize: onPostPage ? '1.15rem' : '1.15rem', letterSpacing: "-0.0135em" }} onClick={() => {}}>
                 <Linkify options={{
