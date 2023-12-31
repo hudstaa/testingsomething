336afba00780ea77f9c4ee1bf185aee0cd720eef
spoonschad <chapo@membot.tools>
2023-12-31 17:55:21 -0500

    df

--- src/components/WriteMessage.tsx
+++ src/components/WriteMessage.tsx
@@ -75,7 +75,7 @@ export const WriteMessage: React.FC<{ placeHolder: string, address: string, send
   const strippedLength = message?.content ? message.content.replaceAll(' ', '').replaceAll('\n', '').length : 0
 
   return (
-    <IonToolbar color={bgColor} style={{ padding: 4, paddingBottom: 36, paddingLeft: 12, paddingRight: 12, border: 0 }} >
+    <IonToolbar color={bgColor} style={{ padding: 4, paddingBottom: 36, paddingLeft: 8, paddingRight: 8, border: 0 }} >
       <div style={{display: 'flex', width: '100%'}}>
       <div style={{backgroundColor: 'var(--ion-color-light)', marginLeft: 0, marginTop: 4,paddingRight: 0, borderRadius: '32px', maxHeight: 52, width: '100%',display: 'flex'}}> 
       {showMediaButton && (
