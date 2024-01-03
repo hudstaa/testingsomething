3bc533827990cae74aa9b2ac1315991aed62aada
spoonschad <chapo@membot.tools>
2024-01-03 15:14:54 -0500

    wd

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -230,7 +230,7 @@ const Posts: React.FC = () => {
                 </IonFab>
             </IonContent >
             <IonFooter color='black'>
-            <div style={{height: '6vh', marginBottom: -2, backgroundColor: "black", display: 'flex', flexDirection: 'row', borderTopLeftRadius: '32px', borderTopRightRadius: '32px', paddingLeft: 8, paddingRight: 8, paddingTop: 6}}> 
+            <div style={{height: '5.8vh', marginBottom: -2, backgroundColor: "black", display: 'flex', flexDirection: 'row', borderTopLeftRadius: '32px', borderTopRightRadius: '32px', paddingLeft: 8, paddingRight: 8, paddingTop: 6}}> 
                 <IonSegment
                     value={filterType}
                     slot='start'
