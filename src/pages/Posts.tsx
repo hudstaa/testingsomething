9daaf6341262f84df2454469825e85ffd9cd2182
spoonschad <chapo@membot.tools>
2024-01-03 15:30:19 -0500

    dv

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -230,21 +230,21 @@ const Posts: React.FC = () => {
                 </IonFab>
             </IonContent >
             <IonFooter color='black'>
-            <div style={{height: '5.8vh', marginBottom: -2, backgroundColor: "black", display: 'flex', flexDirection: 'row', borderTopLeftRadius: '32px', borderTopRightRadius: '32px', paddingLeft: 8, paddingRight: 8, paddingTop: 6}}> 
+            <div style={{height: '6.2vh', marginBottom: -2, paddingBottom: 2, backgroundColor: "black", display: 'flex', flexDirection: 'row', borderTopLeftRadius: '28px', borderTopRightRadius: '28px', paddingLeft: 8, paddingRight: 8, paddingTop: 6}}> 
                 <IonSegment
                     value={filterType}
                     slot='start'
                     className="heavy my-custom-segment-class2"
-                    style={{ fontSize: 24,display: 'flex', paddingLeft: 4, paddingRight: 4, justifyContent: 'space-between', alignItems: 'center', borderRadius: 24}} >
-                    <IonSegmentButton className="my-custom-segment-class2" value="Feed" color={filterType === 'Feed' ? 'white' : undefined}>
-                        <IonLabel className='heavy' color="white" style={{fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Feed</IonLabel>
-                    </IonSegmentButton>
-                    <IonSegmentButton className="my-custom-segment-class2" value="Apps" color={filterType === 'Apps' ? 'white' : undefined}>
-                        <IonLabel className='heavy'color="white"  style={{fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Apps</IonLabel>
-                    </IonSegmentButton>
-                    <IonSegmentButton className="my-custom-segment-class2" value="Wallet" color={filterType === 'Wallet' ? 'white' : undefined}>
-                        <IonLabel className='heavy'color="white"  style={{fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Wallet</IonLabel>
-                    </IonSegmentButton>
+                    style={{ fontSize: 24,display: 'flex', paddingLeft: 2, border: '1px solid #FFFFFF20',paddingRight: 2, justifyContent: 'space-between', alignItems: 'center', borderRadius: 24}} >
+                     <IonSegmentButton value="Feed" onClick={() => setFilterType('Feed')}>
+                            <IonLabel className='heavy' style={{color: filterType === 'Feed' ? 'white' : 'var(--ion-color-medium)', fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Feed</IonLabel>
+                        </IonSegmentButton>
+                        <IonSegmentButton value="Apps" onClick={() => setFilterType('Apps')}>
+                            <IonLabel className='heavy' style={{color: filterType === 'Apps' ? 'white' : 'var(--ion-color-medium)', fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Apps</IonLabel>
+                        </IonSegmentButton>
+                        <IonSegmentButton value="Wallet" onClick={() => setFilterType('Wallet')}>
+                            <IonLabel className='heavy' style={{color: filterType === 'Wallet' ? 'white' : 'var(--ion-color-medium)', fontSize: 16, paddingBottom: 0, paddingTop: 0, paddingLeft: 24, paddingRight: 24}}>Wallet</IonLabel>
+                        </IonSegmentButton>
                 </IonSegment>
             </div> 
         </IonFooter>
