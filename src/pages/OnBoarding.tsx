dbb9858c6ebd22c5aba450588b9a9ade2f1da023
spoonschad <chapo@membot.tools>
2024-01-05 15:05:52 -0500

    dw

--- src/pages/OnBoarding.tsx
+++ src/pages/OnBoarding.tsx
@@ -82,9 +82,6 @@ export const OnBoarding: React.FC<{ me: any, dismiss: () => void }> = ({ me, dis
 
                 </IonButton> : <>
                     <IonButton onClick={dismiss} fill='clear'>
-                        {path === '/auth' ? 'open in tribe' : user.twitter?.name}
-                        {path !== '/auth' ? <IonIcon color="success" icon={checkmark} /> : <img style={{ borderRadius: 12 }} height={40} src={'/favicon.png'} />}
-
                     </IonButton>
                     <br />
                 </>}
