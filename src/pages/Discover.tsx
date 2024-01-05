ad61bcecf96648701a400efb587e8cc65291fd45
spoonschad <chapo@membot.tools>
2024-01-05 05:57:47 -0500

    v

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -111,7 +111,7 @@ const Discover: React.FC = () => {
                     <IonBadge style={{paddingLeft: 6, marginBottom: -6, paddingTop: 0, paddingBottom: 0, fontSize: 16}} color='transparent' >
                       <div style={{display: 'flex', flexDirection: 'column'}}>
                         <span style={{textAlign: 'left', fontSize: '.95rem', paddingTop: 2}}className="medium">{member?.twitterName} </span>
-                        <span className='regular' style={{opacity: 0.75, fontSize: '.9rem', fontWeight: 400, paddingTop: 4}}>{member?.bio}</span>
+                        <span className='medium' style={{opacity: 0.75, fontSize: '.9rem', fontWeight: 500, paddingTop: 4}}>{member?.bio}</span>
                       </div>
                       <div style={{textAlign: 'left'}}>{<BuyPriceBadge address={member?.address} />}</div>
                     </IonBadge>
