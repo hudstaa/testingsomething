f7f593577f9e87bb0943161caac57cc83f3cb007
spoonschad <chapo@membot.tools>
2024-01-08 02:32:02 -0500

    t

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -124,7 +124,7 @@ const Member: React.FC<{ profile: boolean }> = ({ profile }) => {
                     {member && <SubscribeButton topic={member.address} uid={nativeAuth().currentUser?.uid || ""} />}
                 </>}
                 {member && <>
-                    <IonGrid style={{ padding: 0, borderBottom: '1px solid var(--ion-color-medium-shade' }}>
+                    <IonGrid style={{ padding: 0, borderBottom: '1px solid var(--ion-color-medium-shade', backgroundColor: 'var(--ion-color-tabblur)' }}>
                         <IonRow>
                             <IonCol sizeLg='6' sizeXs='12' sizeMd='6' offsetLg='3' offsetMd='3' offsetSm='0' sizeSm='12' style={{ padding: '0px' }}>
                                 <IonSegment className='custom1' color='transparent' mode='md' value={segment}>
